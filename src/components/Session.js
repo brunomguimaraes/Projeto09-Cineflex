10553ae6241bebd433524be059d9d4bad0312c17
Bruno <mguimaraesbruno@gmail.com>
2021-09-10 11:03:22 -0300

    Code Review: Brunão

--- src/components/Session.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function Session({idSessao, time}) {
-    return (
-        <Link to={`/sessao/${idSessao}`}>
-            <li className="time">{time}</li>
-        </Link>    
-    );
-}
